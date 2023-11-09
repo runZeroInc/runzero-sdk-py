6e33c503684359a7ac6b5c9a450b15e970dc338d
Travis Reyburn <38515664+treyburn@users.noreply.github.com>
2023-11-09 11:43:43 -0700

    Release 0.7.3 (#24)
    ## [0.7.3] - 2023-11-09
    
    - Fixed IDE autocompletion naming for the Software and Vulnerability
    classes by modifying the underlying class names which previously did not
    match the exported class name.

--- runzero/errors.py
+++ runzero/errors.py
@@ -14,6 +14,7 @@ Sub-packages and modules may create their own error types in those packages, par
 if they are not useful outside of that package, but should inherit a base type here.
 """
 
+
 class Error(Exception):
     """Error is a named Exception class representing bottom-level runZero Error type.
 
