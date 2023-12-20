bce13d14256a0e5a719127bbc1ff29b37e34e3ba
Travis Reyburn <38515664+treyburn@users.noreply.github.com>
2023-12-20 13:43:22 -0700

    Release v0.8.0 (#25)
    ## [0.8.0] - 2023-12-20
    
    - `runzero.types.ImportAsset` now supports associating services with an
    asset by adding a `runzero.types.Service` type to be set on the
    `ImportAsset.services` field.
    - Updated `certifi` dependency to latest version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.0] - 2023-12-20
+
+- `runzero.types.ImportAsset` now supports associating services with an asset by adding a `runzero.types.Service` type to be set on the `ImportAsset.services` field.
+- Updated `certifi` dependency to latest version.
+
 ## [0.7.3] - 2023-11-09
 
 - Fixed IDE autocompletion naming for the Software and Vulnerability classes by modifying the underlying class names which previously did not match the exported class name.
--- runzero/types/_wrapped.py
+++ runzero/types/_wrapped.py
@@ -23,6 +23,8 @@ from ._data_models_gen import NetworkInterface as RESTNetworkInterface
 from ._data_models_gen import ScanOptions as RESTScanOptions
 from ._data_models_gen import ScanTemplate as RESTScanTemplate
 from ._data_models_gen import ScanTemplateOptions as RESTScanTemplateOptions
+from ._data_models_gen import Service as RESTService
+from ._data_models_gen import ServiceProtocolData as RESTServiceProtocolData
 from ._data_models_gen import Software as RESTSoftware
 from ._data_models_gen import Tag as RESTTag
 from ._data_models_gen import Vulnerability as RESTVulnerability
@@ -261,6 +263,143 @@ class ScanTemplateOptions(RESTScanTemplateOptions):
         """Ensure kebab-case is kept when converting to JSON"""
         kwargs.setdefault("by_alias", True)
         return super().json(*args, **kwargs)
+
+
+class Service(RESTService):
+    """A service running on an asset."""
+
+    __MAX_ATTRS = 1024
+    __MAX_ATTR_KEY_LEN = 256
+    __MAX_ATTR_VAL_LEN = 1024
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    @validator("address", pre=True)
+    def _address_str_conversion(  # pylint: disable=E0213
+        cls, addr: Union[str, IPv4Address, IPv6Address]
+    ) -> Union[IPv4Address, IPv6Address]:
+        """
+        This gives the user the convenience of passing in a valid ipv4/ipv6 as a string and doing type conversion
+        """
+        if isinstance(addr, str):
+            addr = ip_address(addr)
+        return addr
+
+    @validator("transport", pre=True)
+    def _lower_case_transport(cls, attr: str) -> str:  # pylint: disable=E0213
+        # disabled pylint because @validator turns the method into a classmethod
+        """
+        This simply lower-cases transport name for convenience
+        """
+        return attr.lower()
+
+    @validator("custom_attributes", pre=True)
+    def _custom_attributes_length(  # pylint: disable=E0213
+        cls, attrs: Dict[str, Union[CustomAttribute, str]]
+    ) -> Dict[str, str]:
+        # disabled pylint because @validator turns the method into a classmethod
+        """
+        Validates the following:
+        - that the length of the dict used for `custom_attributes` does not exceed a length of 256 items
+        - that each key in that dict does not exceed a length of 256 characters
+        - that the length of each value does not exceed a length of 1024 characters
+        """
+        if len(attrs) > cls.__MAX_ATTRS:
+            raise ValidationError(
+                f"custom attributes exceeds length of 256 with length of {len(attrs)}",
+                ImportAsset,
+            )
+
+        # store for return value after type casting to handle CustomAttribute instances
+        processed_attrs: Dict[str, str] = {}
+
+        for k, val in attrs.items():
+            if len(k) > cls.__MAX_ATTR_KEY_LEN:
+                raise ValidationError(
+                    f"key {k[:25]}... in custom_attributes exceeds maximum length of 256 with length of {len(k)}",
+                    ImportAsset,
+                )
+
+            # CustomAttribute used to be the required type for the custom attributes value field
+            # Now we use strings - but still support CustomAttribute for backwards compatability
+            # Thus we need to cast any CustomAttribute() to a string because the wrapped type uses strings
+            if isinstance(val, CustomAttribute):
+                val = val.__root__
+                attrs[k] = val
+            if len(val) > cls.__MAX_ATTR_VAL_LEN:
+                raise ValidationError(
+                    f"key {k[:25]}... in custom_attributes has a value which the exceeds maximum length of 1024 with"
+                    f" length of {len(str(val))}",
+                    ImportAsset,
+                )
+            processed_attrs[k] = val
+
+        return processed_attrs
+
+
+class ServiceProtocolData(RESTServiceProtocolData):
+    """
+    ServiceProtocolData represents the attributes associated with a given service protocol
+    """
+
+    __MAX_ATTRS = 1024
+    __MAX_ATTR_KEY_LEN = 256
+    __MAX_ATTR_VAL_LEN = 1024
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    @validator("name", pre=True)
+    def _lower_case_name(cls, attr: str) -> str:  # pylint: disable=E0213
+        # disabled pylint because @validator turns the method into a classmethod
+        """
+        This simply lower-cases protocol name for convenience
+        """
+        return attr.lower()
+
+    @validator("attributes", pre=True)
+    def _attributes_length(  # pylint: disable=E0213
+        cls, attrs: Dict[str, Union[CustomAttribute, str]]
+    ) -> Dict[str, str]:
+        # disabled pylint because @validator turns the method into a classmethod
+        """
+        Validates the following:
+        - that the length of the dict used for `custom_attributes` does not exceed a length of 256 items
+        - that each key in that dict does not exceed a length of 256 characters
+        - that the length of each value does not exceed a length of 1024 characters
+        """
+        if len(attrs) > cls.__MAX_ATTRS:
+            raise ValidationError(
+                f"custom attributes exceeds length of 256 with length of {len(attrs)}",
+                ImportAsset,
+            )
+
+        # store for return value after type casting to handle CustomAttribute instances
+        processed_attrs: Dict[str, str] = {}
+
+        for k, val in attrs.items():
+            if len(k) > cls.__MAX_ATTR_KEY_LEN:
+                raise ValidationError(
+                    f"key {k[:25]}... in custom_attributes exceeds maximum length of 256 with length of {len(k)}",
+                    ImportAsset,
+                )
+
+            # CustomAttribute used to be the required type for the custom attributes value field
+            # Now we use strings - but still support CustomAttribute for backwards compatability
+            # Thus we need to cast any CustomAttribute() to a string because the wrapped type uses strings
+            if isinstance(val, CustomAttribute):
+                val = val.__root__
+                attrs[k] = val
+            if len(val) > cls.__MAX_ATTR_VAL_LEN:
+                raise ValidationError(
+                    f"key {k[:25]}... in custom_attributes has a value which the exceeds maximum length of 1024 with"
+                    f" length of {len(str(val))}",
+                    ImportAsset,
+                )
+            processed_attrs[k] = val
+
+        return processed_attrs
 
 
 class Software(RESTSoftware):
